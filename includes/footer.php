076e8df8b3f3defbf39f24a2ddac98841ccfeb01
Watheq <watheq.alshowaiter@gmail.com>
2020-02-27 14:16:37 +0300

    adding the designer link

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 <footer class="footer no-margin" style="text-align: center;">
   <div class="footer-container">
     <p class="no-margin padding-1">كل الحقوق محفوظة لـ <a href="">واثق الشويطر</a>، <?=   date('Y'); ?>.
-      تصميم المدونة بواسطة <a href="">عبداللطيف الرداعي</a>.
+      تصميم المدونة بواسطة <a href="https://www.linkedin.com/in/abdulateef-al-radaee-8101171a1/">عبداللطيف الرداعي</a>.
     </p>
 
   </div>
