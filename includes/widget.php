8187b2828977b49813bcad455f14fa4de265fe3c
Watheq <watheq.alshowaiter@gmail.com>
2020-02-28 22:53:06 +0300

    make my social account _blank

--- includes/widget.php
+++ includes/widget.php
@@ -3,11 +3,11 @@
     <h4 class="no-margin in-block">حساباتي الاجتماعية</h4>
   </div>
   <div class="icon-container padding-1">
-    <a href="https://www.facebook.com/watheq.show"> <i class="fab fa-facebook fa-3x"> </i></a> &nbsp;
-    <a href="https://twitter.com/watheq_show"><i class="fab fa-twitter fa-3x"> </i></a> &nbsp;
-    <a href="https://github.com/watheqAlshowaiter"><i class="fab fa-github fa-3x"></i></a> &nbsp;
-    <a href="https://www.linkedin.com/in/watheq-show/"><i class="fab fa-linkedin fa-3x"></i></a> &nbsp;
-    <a href="https://ar.quora.com/profile/Watheq-Alshowaiter"><i class="fab  fa-quora fa-3x"></i></a> &nbsp;
+    <a target="_blank" href="https://www.facebook.com/watheq.show"> <i class="fab fa-facebook fa-3x"> </i></a> &nbsp;
+    <a target="_blank" href="https://twitter.com/watheq_show"><i class="fab fa-twitter fa-3x"> </i></a> &nbsp;
+    <a target="_blank" href="https://github.com/watheqAlshowaiter"><i class="fab fa-github fa-3x"></i></a> &nbsp;
+    <a target="_blank" href="https://www.linkedin.com/in/watheq-show/"><i class="fab fa-linkedin fa-3x"></i></a> &nbsp;
+    <a target="_blank" href="https://ar.quora.com/profile/Watheq-Alshowaiter"><i class="fab  fa-quora fa-3x"></i></a> &nbsp;
 
   </div>
 </div>
