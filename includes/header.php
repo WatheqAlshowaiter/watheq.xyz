cb7e46ebabdceb83af238e0f4292f13d2d8c4054
Watheq <watheq.alshowaiter@gmail.com>
2020-02-29 13:52:37 +0300

    تغيير النسخ لتحسين الstyle

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
     <link rel="shortcut icon" href="images/icon/icon.png?v=2">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/style.css?v=0.1">
+    <link rel="stylesheet" type="text/css" href="css/style.css?v=2">
 
   <body class="no-margin">
     <div class="page-container">
