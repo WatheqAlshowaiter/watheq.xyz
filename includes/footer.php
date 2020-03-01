43ff73ac616ffe08e49449e16a95f2e695d6987c
Watheq <watheq.alshowaiter@gmail.com>
2020-03-01 12:31:12 +0300

    fix the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,11 @@
   <div id="top-button-container">
-    <span class="fa fa-arrow-up" title="Go to top" onclick=" ()"></span>
+    <span class="fa fa-arrow-up" title="Go to top" onclick="topFunction()"></span>
   </div>
   <footer class="footer no-margin" style="text-align: center;">
     <div class="footer-container">
 
       <p class="no-margin padding-2">
-        </a> محتوى هذه المدونة مرخص بموجب <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/deed.ar">رخصة المشاع الإبداعي نَسب المُصنَّف - الترخيص بالمثل 4.0 دولي</a>. برمجة ومحتوى واثق الشويطر. تصميم <a href="https://www.linkedin.com/in/abdulateef-al-radaee-8101171a1/" target="_blank">عبداللطيف الرداعي</a> <?= date('Y'); ?>.
+        </a> محتوى المدونة مرخص بموجب <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/deed.ar">رخصة المشاع الإبداعي نَسب المُصنَّف - الترخيص بالمثل 4.0 دولي</a>. برمجة ومحتوى واثق الشويطر. تصميم <a href="https://www.linkedin.com/in/abdulateef-al-radaee-8101171a1/" target="_blank">عبداللطيف الرداعي</a> <?= date('Y'); ?>.
       </p>
 
 
