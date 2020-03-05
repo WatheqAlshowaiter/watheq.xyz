7161e1cacdb5f9facca58b6578efecb1d49bb64b
Watheq <watheq.alshowaiter@gmail.com>
2020-03-05 15:34:00 +0300

    fix disuess issue in post.php

--- post.php
+++ post.php
@@ -71,7 +71,7 @@ if (isset($_GET['p_id'])) {
        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
 
       var disqus_config = function() {
-        this.page.url = <?php echo "http://www.watheq.xyz/post.php?p_id=" . $post_id . ""; ?>; // Replace PAGE_URL with your page's canonical URL variable
+        // this.page.url = <?='http://www.watheq.xyz/post.php?p_id=' . $post_id ;?>// Replace PAGE_URL with your page's canonical URL variable
         this.page.identifier = <?= $post_id; ?>; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
       };
 
