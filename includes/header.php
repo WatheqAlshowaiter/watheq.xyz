417b051a07785635ed5d6d66cd0f0aca86caeb66
Watheq <watheq.alshowaiter@gmail.com>
2020-03-18 15:47:52 +0300

    fixing icon to appear

--- includes/header.php
+++ includes/header.php
@@ -31,12 +31,14 @@
 
   <meta name="twitter:image:alt" content="مدونة واثق">
 
+
   <meta property="og:locale" content="ar_AR" />
+
   <meta name="twitter:creator" content="@watheq_show" />
   <meta name="twitter:site" content="@watheq_show" />
-   
+  <link rel="shortcut icon" href="images/icon/icon.png?v=3">  
   <title>مدونة واثق <? echo isset($post_title) ? ' - ' . $post_title : ''; ?></title>
-  <link rel="shortcut icon" href="images/icon/icon.png?v=2">
+
 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/style.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
