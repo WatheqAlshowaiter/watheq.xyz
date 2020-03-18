b422bb2b0e8bf64eb4816ed00e1af946e71ff41a
Watheq <watheq.alshowaiter@gmail.com>
2020-03-18 12:06:14 +0300

    adding sitemap and robots.txt

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name=”robots” content=”noindex,nofollow”>
 
     <title>SB Admin - Dashboard</title>
 
